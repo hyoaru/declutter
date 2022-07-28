b7f2456094411604fc639e0cddc0afdfd9575e27
hyoaru <jjcabreraaaa@gmail.com>
2022-07-28 21:45:25 +0800

    [add] database models

---
+++ declutter/blueprints/models/posts.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from declutter import db
+
+class Posts(db.Model):
+    __tablename__ = 'posts'
+    post_id = db.Column(db.Integer, primary_key = True)
+    post_user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable = False)
+    post_title = db.Column(db.String(100), nullable = False)
+    post_content = db.Column(db.Text, nullable = False)
+    post_date = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+
+    def __repr__(self):
+        return f"Posts('{self.post_title}', '{self.post_date}'"
+
+
---
+++ declutter/blueprints/models/users.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from declutter import db
+
+class Users(db.Model):
+    __tablename__ = 'users'
+    user_id = db.Column(db.Integer, primary_key = True)
+    user_email = db.Column(db.String(200), nullable = False, unique = True)
+    user_username = db.Column(db.String(40), nullable = False, unique = True)
+    user_password = db.Column(db.String(40), nullable = False)
+    user_date_created = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    posts = db.relationship('Posts', backref = 'post_author', lazy = True)
+
+    def __repr__(self):
+        return f"User('{self.user_username}', '{self.user_email}'"
