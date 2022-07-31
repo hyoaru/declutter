51f21621fc9dfa6011e7bae2bbda09747ccbc10e
hyoaru <jjcabreraaaa@gmail.com>
2022-07-31 11:47:26 +0800

    [modify] refactor app

--- declutter/__init__.py
+++ declutter/__init__.py
@@ -1,18 +1,26 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from declutter.database import db
 
-app = Flask(
-    __name__, 
-    template_folder='templates',
-    static_folder='static',
-)
+def create_app():
+    load_dotenv()
+    app = Flask(
+        __name__, 
+        template_folder='templates',
+        static_folder='static',
+    )
+    
+    app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
 
-load_dotenv()
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
+    return app
 
-db = SQLAlchemy(app)
+app = create_app()
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+    print(db.engine.table_names())
 
 from declutter import routes
