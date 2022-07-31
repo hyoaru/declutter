b0e03bdbfe51b7dd43c7c016d912281586e549ea
hyoaru <jjcabreraaaa@gmail.com>
2022-07-31 14:11:24 +0800

    [add] flask bcrpyt

--- declutter/__init__.py
+++ declutter/__init__.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask
-from declutter.database import db
+from declutter.database import db, bcrypt
 
 def create_app():
     load_dotenv()
@@ -10,14 +10,16 @@ def create_app():
         template_folder='templates',
         static_folder='static',
     )
-    
+
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
     return app
 
 app = create_app()
 db.init_app(app)
+bcrypt.init_app(app)
 
 with app.app_context():
     db.create_all()
--- declutter/database.py
+++ declutter/database.py
@@ -1,5 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 # Import database models
 from declutter.blueprints.models.users import Users
