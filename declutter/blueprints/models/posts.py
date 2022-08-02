3d36efee6aadfd04cf82cc4c3cdfb9333212965e
hyoaru <jjcabreraaaa@gmail.com>
2022-08-02 19:59:31 +0800

    [remove] date_created_local column

--- declutter/blueprints/models/posts.py
+++ declutter/blueprints/models/posts.py
@@ -1,5 +1,6 @@
 import pytz
 from datetime import datetime, tzinfo
+from dateutil import tz
 from declutter.database import db
 
 class Posts(db.Model):
@@ -8,8 +9,8 @@ class Posts(db.Model):
     post_user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable = False)
     post_title = db.Column(db.String(100), nullable = False)
     post_content = db.Column(db.Text, nullable = False)
-    post_date = db.Column(db.DateTime, nullable = False, default = datetime.now(pytz.utc))
+    post_date_created_utc = db.Column(db.DateTime, nullable = False, default = datetime.now(pytz.utc))
 
     def __repr__(self):
-        return f"Posts('{self.post_id}', '{self.post_title}', '{self.post_date}', '{self.post_author}'"
+        return f"Posts('{self.post_id}', '{self.post_title}', '{self.post_date_created_utc}', '{self.post_author}'"
 
--- declutter/blueprints/models/users.py
+++ declutter/blueprints/models/users.py
@@ -1,5 +1,6 @@
 import pytz
 from datetime import datetime, tzinfo
+from dateutil import tz
 from declutter.database import db, login_manager
 from flask_login import UserMixin
 
@@ -13,11 +14,11 @@ class Users(db.Model, UserMixin):
     user_email = db.Column(db.String(200), nullable = False, unique = True)
     user_username = db.Column(db.String(40), nullable = False, unique = True)
     user_password = db.Column(db.String(40), nullable = False)
-    user_date_created = db.Column(db.DateTime, nullable = False, default = datetime.now(pytz.UTC))
+    user_date_created_utc = db.Column(db.DateTime, nullable = False, default = datetime.now(pytz.UTC))
     posts = db.relationship('Posts', backref = 'post_author', lazy = True)
 
     def __repr__(self):
-        return f"User('{self.user_id}', {self.user_email}', '{self.user_username}', '{self.user_date_created}')"
+        return f"User('{self.user_id}', {self.user_email}', '{self.user_username}', '{self.user_date_created_utc}')"
 
     def get_id(self):
         return self.user_id
