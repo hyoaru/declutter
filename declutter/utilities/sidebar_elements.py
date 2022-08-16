0ded0fc6ca03c3b57de5e56eef20a6799ff5251f
hyoaru <jjcabreraaaa@gmail.com>
2022-08-16 21:17:33 +0800

    [add] sidebar dailyquote

--- declutter/utilities/sidebar_elements.py
+++ declutter/utilities/sidebar_elements.py
@@ -1,4 +1,8 @@
+import os
+import pickle
+import requests
 from typing import Type
+from datetime import datetime, timedelta
 
 # Database models
 from declutter.models.posts import Posts
@@ -6,9 +10,71 @@ from declutter.models.users import Users
 
 
 def get_posts_recent_10() -> Type[Posts]:
+    """Returns the recent 10 posts."""
+
     posts = (
         Posts.query
         .order_by(Posts.post_date_created_utc.desc())
         .limit(10).all())
 
     return posts
+
+
+def get_users_recent_10() -> Type[Users]:
+    """Returns the 10 recently joined users."""
+
+    users = (
+        Users.query
+        .order_by(Users.user_date_created_utc.desc())
+        .limit(10).all())
+
+    return users
+
+
+def saveto_binary(obj_structure, file_name: str, data_root_path: str = None) -> None:
+    """Saves an object structure to binary format."""
+
+    data_root_path = 'declutter/data' if data_root_path is None else data_root_path
+
+    if not os.path.exists(f'{data_root_path}'):
+        os.mkdir(f'{data_root_path}')
+
+    with open(f'{data_root_path}/{file_name}.bin', 'wb') as file:
+        pickle.dump(obj_structure, file)
+
+
+def load_binary(file_name: str, data_root_path: str = None):
+    """Loads and returns a binary object structure."""
+
+    data_root_path = 'declutter/data' if data_root_path is None else data_root_path
+
+    if os.path.exists(f'{data_root_path}/{file_name}.bin'):
+        file = open(f'{data_root_path}/{file_name}.bin', 'rb')
+        obj_structure = pickle.load(file)
+        file.close()
+
+        return obj_structure
+
+    else:
+        return {}
+
+
+def get_daily_random_quotes(api: str = "https://api.quotable.io/random?tags=famous-quotes"):
+    """Returns daily quote from an api if the dateToReload value of the quote dictionary is greater than utc date now."""
+
+    quote = load_binary(file_name = 'sidebar_daily_quote')
+
+    quote_date_to_reload = quote.get('dateToReload')
+    if quote_date_to_reload is not None and quote_date_to_reload > datetime.utcnow().date():
+        return quote
+    else:
+        try:
+            request = requests.get(api)
+        except:
+            return {}
+
+        quote = request.json()[0] if type(request.json()) == type([]) else request.json()
+        quote.update({'dateToReload': datetime.utcnow().date() + timedelta(days = 1)})
+        saveto_binary(obj_structure = quote, file_name = 'sidebar_daily_quote')
+
+        return quote
--- run.py
+++ run.py
@@ -1,13 +1,19 @@
+from datetime import datetime
+
+# App imports
 from declutter import create_app
 from declutter.utilities.datetime import datetime_tolocal
-from declutter.utilities.sidebar_elements import get_posts_recent_10
+from declutter.utilities.sidebar_elements import get_posts_recent_10, get_users_recent_10, get_daily_random_quotes
 
 if __name__ == "__main__":
     app = create_app()
 
     @app.context_processor
-    def inject_sidebar_elements():
-        return dict(datetime_tolocal = datetime_tolocal, sidebar_posts = get_posts_recent_10())
+    def inject_global_elements():
+        return dict(
+            datetime_tolocal = datetime_tolocal, datetime_utcnow = datetime.utcnow(), 
+            posts_recent_10 = get_posts_recent_10(), users_recent_10 = get_users_recent_10(), 
+            quote_of_the_day = get_daily_random_quotes())
 
     app.run(debug=True)
  
