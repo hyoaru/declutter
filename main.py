55418e929497a0ea12cded6a94116061224d18e5
hyoaru <jjcabreraaaa@gmail.com>
2023-04-15 12:50:07 +0800

    [modify] renamed run py -> main py

--- main.py
+++ main.py
@@ -6,8 +6,4 @@ from declutter import create_app
 from declutter.utilities.datetime import datetime_tolocal
 from declutter.utilities.sidebar_elements import get_posts_recent_n, get_users_recent_n, get_daily_random_quotes
 
-# if __name__ == "__main__":
-#     app = create_app()
-#     app.run(debug=True)
- 
 app = create_app()
